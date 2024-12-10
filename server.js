26cc9e542a314de8919e29fb1822e863d2e4c2f4
Glitch (glitch-hello-node) <none>
2024-12-10 11:18:58 +0000

    💡🌝 Checkpoint ./server.js:62919950/983 ./.txt:62919950/2

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const WebSocket = require('ws');
 const express = require('express');
 const fs = require("fs");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile('/app/index.html');
+});
+app.get('/*', (req, res) => {
+  res.sendFile('/app'+req.url)
+});
+
+const server = app.listen(3000, () => {
+  console.log(`up`);
+});
