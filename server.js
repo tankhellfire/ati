1b17266925b99a73eb98ae061a7465efdd60311a
Glitch (glitch-hello-node) <none>
2024-12-14 02:22:13 +0000

    🎂😎 Checkpoint ./server.js:62919950/4495

--- server.js
+++ server.js
@@ -7,35 +7,37 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*").sendFile('/app/index.html');
-});
+}
+);
 
 app.get("/*", (req, res) => {
-  function readDir(requestedPath){
-              fs.readdir(requestedPath, (err, files) => {
-            if (err) {
-              res.status(500).send(`Error reading directory"${err}"`);
-              return
-            }
+  function readDir(requestedPath, dirUrl) {
+    fs.readdir(requestedPath, (err, files) => {
+      if (err) {
+        res.status(500).send(`Error reading directory"${err}"`);
+        return
+      }
 
-            // Return the list of files
-            res.send(`<body
-    style="
-      margin: 0;
-      height: 100vh;
-      width: 100vw;
-      background-color: #000;
-      color: #fff;
-      font-family: Consolas, monospace;
-    ">${files.map((file) =>`<a href="${req.path}">${file}</a>`).join("<br>")}`);
-          });
+      // Return the list of files
+      res.send(`<body style="
+        margin: 0;
+        height: 100vh;
+        width: 100vw;
+        background-color: #000;
+        color: #fff;
+        font-family: Consolas, monospace;
+       ">${
+        files.map( (file) => `<a href="${dirUrl + '/' + file
+       }">${file}</a>`).join("<br>")}`);
+    }
+    );
   }
-  
-  
-  let dirPath=req.path.split('/')
-  if(dirPath.pop()==="index"){
-    return readDir(path.join(__dirname,...dirPath))
+
+  let dirPath = req.path.split('/')
+  if (dirPath.pop() === "index") {
+    return readDir(path.join(__dirname, ...dirPath), req.path.split('/').slice(0, -1).join('/'))
   }
-  
+
   const requestedPath = path.join(__dirname, req.path);
 
   fs.stat(requestedPath, (err, stats) => {
@@ -49,18 +51,22 @@ app.get("/*", (req, res) => {
       const indexPath = path.join(requestedPath, "index.html");
       fs.access(indexPath, fs.constants.F_OK, (err) => {
         if (err) {
-          return readDir(requestedPath)
+          return readDir(requestedPath, req.path)
         } else {
           return res.sendFile(indexPath);
         }
-      });
+      }
+      );
     } else {
       // If it's not a directory, serve the file
       res.sendFile(requestedPath);
     }
-  });
-});
+  }
+  );
+}
+);
 
 const server = app.listen(3000, () => {
   console.log(`up`);
-});
+}
+);
