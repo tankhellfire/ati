4143200bf2d6ae9aacd5f409f8032081edb1ac74
Glitch (glitch-hello-node) <none>
2024-12-17 23:18:06 +0000

    🎟⛲️ Checkpoint ./server.js:62919950/912 ./lib/lib.js:62919950/1813 ./package.json:62919950/1012

--- server.js
+++ server.js
@@ -1,73 +1,83 @@
-const WebSocket = require('ws');
-const express = require('express');
-const path = require("path");
-const fs = require("fs");
+(async()=>{
+  const WebSocket = require('ws');
+  const express = require('express');
+  const path = require("path");
+  const fs = require("fs");
+  
+  const lib=require("/app/lib/lib.js")
+  
+  console.log(await(lib('spaceGame7/index.js')))
 
-const app = express();
+  const app = express();
 
-app.get('/', (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*").sendFile('/app/index.html');
-}
-);
+  app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*").sendFile('/app/index.html');
+  }
+  );
+  
+  app.get("/game", (req, res) => {
+    
+  })
 
-app.get("/*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  function readDir(requestedPath, dirUrl) {
-    fs.readdir(requestedPath, (err, files) => {
+  app.get("/*", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    function readDir(requestedPath, dirUrl) {
+      fs.readdir(requestedPath, (err, files) => {
+        if (err) {
+          res.status(500).send(`Error reading directory"${err}"`);
+          return
+        }
+
+        // Return the list of files
+        res.send(`<body style="
+          margin: 0;
+          height: 100vh;
+          width: 100vw;
+          background-color: #000;
+          color: #fff;
+          font-family: Consolas, monospace;
+         ">${
+          files.map( (file) => `<a href="${dirUrl + '/' + file
+         }">${file}</a>`).join("<br>")}<br><br><a href="..">..</a>`);
+      }
+      );
+    }
+
+    let dirPath = req.path.split('/')
+    if (dirPath.pop() === "index") {
+      return readDir(path.join(__dirname, ...dirPath), req.path.split('/').slice(0, -1).join('/'))
+    }
+
+    const requestedPath = path.join(__dirname, req.path);
+
+    fs.stat(requestedPath, (err, stats) => {
       if (err) {
-        res.status(500).send(`Error reading directory"${err}"`);
+        //not a path
+        res.status(404).sendFile(path.join(__dirname, "idk.html"));
         return
       }
 
-      // Return the list of files
-      res.send(`<body style="
-        margin: 0;
-        height: 100vh;
-        width: 100vw;
-        background-color: #000;
-        color: #fff;
-        font-family: Consolas, monospace;
-       ">${
-        files.map( (file) => `<a href="${dirUrl + '/' + file
-       }">${file}</a>`).join("<br>")}<br><br><a href="..">..</a>`);
+      if (stats.isDirectory()) {
+        const indexPath = path.join(requestedPath, "index.html");
+        fs.access(indexPath, fs.constants.F_OK, (err) => {
+          if (err) {
+            return readDir(requestedPath, req.path)
+          } else {
+            return res.sendFile(indexPath);
+          }
+        }
+        );
+      } else {
+        // If it's not a directory, serve the file
+        res.sendFile(requestedPath);
+      }
     }
     );
   }
-
-  let dirPath = req.path.split('/')
-  if (dirPath.pop() === "index") {
-    return readDir(path.join(__dirname, ...dirPath), req.path.split('/').slice(0, -1).join('/'))
-  }
-
-  const requestedPath = path.join(__dirname, req.path);
-
-  fs.stat(requestedPath, (err, stats) => {
-    if (err) {
-      //not a path
-      res.status(404).sendFile(path.join(__dirname, "idk.html"));
-      return
-    }
-
-    if (stats.isDirectory()) {
-      const indexPath = path.join(requestedPath, "index.html");
-      fs.access(indexPath, fs.constants.F_OK, (err) => {
-        if (err) {
-          return readDir(requestedPath, req.path)
-        } else {
-          return res.sendFile(indexPath);
-        }
-      }
-      );
-    } else {
-      // If it's not a directory, serve the file
-      res.sendFile(requestedPath);
-    }
-  }
   );
-}
-);
 
-const server = app.listen(3000, () => {
-  console.log(`up`);
-}
-);
+  const server = app.listen(3000, () => {
+    console.log(`up`);
+  }
+  )
+})()
