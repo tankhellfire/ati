dd4e9b2ebb7819da69d8cca7a6f9818b0e8b3d00
Glitch (glitch-hello-node) <none>
2024-12-20 04:52:38 +0000

    🌃⛩ Checkpoint ./.js:62919950/5414

--- .js
+++ .js
@@ -1,57 +1,19 @@
-let start = 1;
-connection = new WebSocket(url);
+const ws = new WebSocket("https://spot-brook-hall.glitch.me");
 
-connection.onopen = () => {
-    connection.send(JSON.stringify(data))
+ws.onopen = async()=>{
+  ws.send("hi")
   console.log("Connected to the server");
-
-
-  connection.onerror = (error) => {
-    console.error("WebSocket Error:", error);
-    connect();
-  };  
-
-  connection.onclose = (event) => {
-    console.warn(
-      "WebSocket connection closed:",
-      event.code,
-      event.reason
-    );
-    connect();
+  
+  ws.onmessage=async(req)=>{
+    console.log("WebSocket onmessage",req)
   };
 
-  connection.onmessage = (e) => {
-    if (start) {
-      start = 0;
-      e.data.arrayBuffer().then((data) => {
-        res = new Uint8Array(data);
 
-        let a = 0;
-        for (let i = 0; i < canvas.width * canvas.height * 4; i++) {
-          if (i % 4 == 3) {
-            img[i] = 255;
-            continue;
-          }
-          img[i] = res[a];
-          a++;
-        }
-        draw();
-      });
-    } else {
-      e.data.arrayBuffer().then((res) => {
-        let b = new Uint8Array(res);
-        let a = readNBits(b, 0, bitcanva);
-        // console.log(a)
-
-        // console.log(b.length)
-        drawPx(
-          a,
-          readNBits(b, bitcanva, 8),
-          readNBits(b, bitcanva + 8, 8),
-          readNBits(b, bitcanva + 16, 8)
-        );
-      });
-    }
+  
+  ws.onerror = async(err)=>{
+    console.error("WebSocket onerror", err);
+  };  
+  ws.onclose = async(err)=>{
+    console.warn("WebSocket onclose",err);
   };
 };
-}
