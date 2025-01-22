2a0a4169f1075c7e4519f37db6d14cb52171cf20
Glitch (glitch-hello-node) <none>
2025-01-22 09:09:35 +0000

    🍰🤘 Checkpoint ./server.js:62919950/2336 ./.js:62919950/120

--- .js
+++ .js
@@ -1,19 +1,13 @@
 const ws = new WebSocket("https://spot-brook-hall.glitch.me");
 
-ws.onopen = async()=>{
+ws.onopen=async()=>{
   ws.send("hi")
   console.log("Connected to the server");
   
-  ws.onmessage=async(req)=>{
+  ws.onmessage=req=>{
     console.log("WebSocket onmessage",req)
   };
 
-
-  
-  ws.onerror = async(err)=>{
-    console.error("WebSocket onerror", err);
-  };  
-  ws.onclose = async(err)=>{
-    console.warn("WebSocket onclose",err);
-  };
+  ws.onerror=console.error
+  ws.onclose=console.warn
 };
--- server.js
+++ server.js
@@ -1,123 +1,41 @@
 (async()=>{
   const WebSocket = require('ws');
-  const express = require('express');
+  const app = require('express')();
   const path = require("path");
   const fs = require("fs");
+  
   global.fetch=require('node-fetch')
-  
-  const lib=await Object.getPrototypeOf(async()=>{}).constructor('exports',await(await fetch('https://tankhellfire.glitch.me/lib/lib.js')).text()+';\nreturn exports')({})
+  global.lib=await Object.getPrototypeOf(async()=>{}).constructor('exports',await(await fetch('https://tankhellfire.glitch.me/lib/lib.js')).text()+';\nreturn exports')({})
 
-  
-  console.log(lib)
-  
-  console.log(await lib('spaceGame7/index.js'))
-
-  const app = express();
-
-  app.get('/', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*").sendFile('/app/index.html');
-  });
-  
-  app.get("/game", (req, res) => {
-    
-  })
-
-  app.get("/*", (req, res) => {
+  app.get("/*",(req,res)=>{
     res.setHeader("Access-Control-Allow-Origin", "*")
-    function readDir(requestedPath, dirUrl) {
-      fs.readdir(requestedPath, (err, files) => {
-        if (err) {
-          res.status(500).send(`Error reading directory"${err}"`);
-          return
-        }
-
-        // Return the list of files
-        res.send(`<body style="
-          margin: 0;
-          height: 100vh;
-          width: 100vw;
-          background-color: #000;
-          color: #fff;
-          font-family: Consolas, monospace;
-         ">${
-          files.map( (file) => `<a href="${dirUrl + '/' + file
-         }">${file}</a>`).join("<br>")}<br><br><a href="..">..</a>`);
-      }
-      );
-    }
-
-    let dirPath = req.path.split('/')
-    if (dirPath.pop() === "index") {
-      return readDir(path.join(__dirname, ...dirPath), req.path.split('/').slice(0, -1).join('/'))
-    }
-
-    const requestedPath = path.join(__dirname, req.path);
-
-    fs.stat(requestedPath, (err, stats) => {
-      if (err) {
-        //not a path
-        res.status(404).sendFile(path.join(__dirname, "idk.html"));
-        return
-      }
-
-      if (stats.isDirectory()) {
-        const indexPath = path.join(requestedPath, "index.html");
-        fs.access(indexPath, fs.constants.F_OK, (err) => {
-          if (err) {
-            return readDir(requestedPath, req.path)
-          } else {
-            return res.sendFile(indexPath);
-          }
-        }
-        );
-      } else {
-        // If it's not a directory, serve the file
-        res.sendFile(requestedPath);
-      }
-    }
-    );
-  }
-  );
-
-  const server = app.listen(3000, () => {
-    console.log(`up`);
-  }
-  )
+  });  
   
   
-  
-  
+  const server = app.listen(3000,e=>console.log(`up`));
   
   const wss = new WebSocket.Server({ server });
   let id=0
   wss.on('connection',(ws,req)=>{
-    //console.log(ws,req)
+    
     ws.id = id++;
     console.log('A new client connected!',ws.id,'@',req.url);
 
-    let info
-    ws.on('message',(message)=>{
-      if(info===undefined){
-        info=1
-      }
-      console.log(`Received:'${message}' from ${ws.id}`);
-
+    ws.on('message',msg=>{
+      console.log(`Received:'${msg}' from ${ws.id}`);
       
       wss.clients.forEach((client)=>{
         if (client.readyState === WebSocket.OPEN && client.id != ws.id) {
-          client.send(message);
+          client.send(msg);
         }
       });
-      
     });
     
-    ws.onerror=(err)=>{
-      console.log(err)
-    }
-    ws.onclose=(err)=>{
-      console.log(err)
-    }
+    ws.onerror=console.error
+    ws.onclose=console.log
     
     ws.send('connected');
   });
+  
+  
 })()
