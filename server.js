60a6576d558bed2d158e411aec8b951dd2416ad8
Glitch (glitch-hello-node) <none>
2025-01-25 02:02:34 +0000

    🙅👫 Checkpoint ./package.json:64712700/102 ./server.js:64712700/855

--- server.js
+++ server.js
@@ -1,41 +1,17 @@
 (async()=>{
-  const WebSocket = require('ws');
+  //const WebSocket = require('ws');
   const app = require('express')();
-  const path = require("path");
-  const fs = require("fs");
+  //const path = require("path");
+  //const fs = require("fs");
   
   global.fetch=require('node-fetch')
-  global.lib=await Object.getPrototypeOf(async()=>{}).constructor('exports',await(await fetch('https://tankhellfire.glitch.me/lib/lib.js')).text()+';\nreturn exports')({})
 
-  app.get("/*",(req,res)=>{
-    res.setHeader("Access-Control-Allow-Origin", "*")
+  app.post("/*",(req,res)=>{
+    //res.setHeader("Access-Control-Allow-Origin", "*")
+    console.log(req)
   });  
   
   
   const server = app.listen(3000,e=>console.log(`up`));
-  
-  const wss = new WebSocket.Server({ server });
-  let id=0
-  wss.on('connection',(ws,req)=>{
-    
-    ws.id = id++;
-    console.log('A new client connected!',ws.id,'@',req.url);
-
-    ws.on('message',msg=>{
-      console.log(`Received:'${msg}' from ${ws.id}`);
-      
-      wss.clients.forEach((client)=>{
-        if (client.readyState === WebSocket.OPEN && client.id != ws.id) {
-          client.send(msg);
-        }
-      });
-    });
-    
-    ws.onerror=console.error
-    ws.onclose=console.log
-    
-    ws.send('connected');
-  });
-  
   
 })()
