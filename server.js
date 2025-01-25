31029745e58a87b2674b6258454e4bb77cd2aec8
Glitch (glitch-hello-node) <none>
2025-01-25 03:02:01 +0000

    🍧🍔 Checkpoint ./server.js:64712700/2134 ./package.json:64712700/208

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
-(async()=>{
-  //const WebSocket=require('ws');
-  const express=require('express');
-  const app=express()
-  //const path=require("path");
-  //const fs=require("fs");
-  
-  
-  app.use(express.json());
-  app.use(express.urlencoded({extended:true}));
-  
-  global.fetch=require('node-fetch')
+const express=require('express');
+const crypto=require('crypto');
 
-  app.post("/*",(req,res)=>{
-    console.log('path:',req.path);
-    console.log('Headers:',Object.keys(req.headers));
-    console.log('Body:',req.body);
-    
-    if (req.body.type===1) {
-      console.log('pong')
-      return res.json({type:1}); // Respond to ping with type 1
-    }
-  });  
+const app=express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.post("/interactions",(req,res)=>{
+  console.log('path:',req.path);
+  console.log('Headers:',Object.keys(req.headers));
+  console.log('Body:',req.body);
   
+
+  if(!verifySignature(
+    req.headers['x-signature-ed25519'],
+    req.headers['x-signature-timestamp'],
+    JSON.stringify(req.body)
+  )){
+    console.error('Signature verification failed');
+    return res.status(401).send('Invalid signature');
+  }
+
+  if(req.body.type===1){
+    console.log('pong')
+    return res.json({type:1}); // Respond to ping with type 1
+  }
+});  
+
+
+const server=app.listen(3000,e=>console.log(`up`));
+
+
+
+
+
+function verifySignature(signature, timestamp, body) {
+  const data = timestamp + body;
+  const key = Buffer.from(process.env.DISCORD_PUBLIC_KEY, 'hex');
+  const signedData = Buffer.from(signature, 'hex');
+  const hmac = crypto.createHmac('sha256', key);
+  hmac.update(data);
+  const expectedSignature = hmac.digest();
   
-  const server=app.listen(3000,e=>console.log(`up`));
-  
-})()
+  console.log
+
+  return signedData.equals(expectedSignature);
+}
