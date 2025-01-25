3ee47fbe215bf3d0e0ab292312dbaf1fdb00b76c
Glitch (glitch-hello-node) <none>
2025-01-25 02:38:48 +0000

    🌇✂️ Checkpoint ./server.js:64712700/1196

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 (async()=>{
-  //const WebSocket = require('ws');
-  const app = require('express')();
-  //const path = require("path");
-  //const fs = require("fs");
+  //const WebSocket=require('ws');
+  const express=require('express');
+  const app=express()
+  //const path=require("path");
+  //const fs=require("fs");
+  
+  
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}));
   
   global.fetch=require('node-fetch')
 
-  app.post("/*",(req,res)=>{
-    //res.setHeader("Access-Control-Allow-Origin", "*")
-    console.log(req)
+  app.post("/*",async(req,res)=>{
+    console.log('path:',req.path);
+    console.log('Headers:',req.headers);
+    console.log('Body:',req.body);
+    
+    res.send({ message: 'Received' });
   });  
   
   
-  const server = app.listen(3000,e=>console.log(`up`));
+  const server=app.listen(3000,e=>console.log(`up`));
   
 })()
