17d4ca019385e5d3bbe4b45dddafad86f7d64204
Glitch (kana-counting-discord-bot) <none>
2025-01-26 04:32:16 +0000

    🍪🏛 Checkpoint ./server.js:64712700/355

--- server.js
+++ server.js
@@ -184,13 +184,23 @@ ws.on('message',async msg=>{
     let index=kana.flat().indexOf(req.d.content)
     
     if(index===-1){
+      
       await reactToMsg(req.d.channel_id,req.d.id,'batsu:1332527544234278995')
-    }else if(kana[index-1]==req.d.content){
+      
+    }else if(kana.flat()[index-1]==save[req.d.guild_id].last){
+      
       (save[req.d.guild_id]??(save[req.d.guild_id]={})).last=req.d.content
       await reactToMsg(req.d.channel_id,req.d.id,'maru:1332527322909245580')
+      
     }else{
-      (save[req.d.guild_id]??(save[req.body.guild_id]={})).last=req.d.content
+      
+      console.log(index,kana.flat()[index-1],save[req.d.guild_id].last);
+      
+      (save[req.d.guild_id]??(save[req.d.guild_id]={})).last=req.d.content
+      await reactToMsg(req.d.channel_id,req.d.id,'batsu:1332527544234278995')
+      await sleep(500)
       await reactToMsg(req.d.channel_id,req.d.id,'hanmaru:1332926614832545793')
+      
     }
     return updateSave()
   }
